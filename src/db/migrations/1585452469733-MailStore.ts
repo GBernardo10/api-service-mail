bb6955f7bb6bd488a8094940938ad5a887a8d18e
GBernardo10 <g_bernardo10@hotmail.com>
2020-03-29 00:44:23 -0300

    setup

---
+++ src/db/migrations/1585452469733-MailStore.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class MailStore1585452469733 implements MigrationInterface {
+  private table = new Table({
+    name: 'mails_store',
+    columns: [
+      {
+        name: 'mail_id',
+        type: 'varchar',
+        generationStrategy: 'uuid',
+        isPrimary: true,
+        isGenerated: true,
+      },
+      {
+        name: 'to',
+        type: 'varchar',
+        isNullable: false,
+      },
+      {
+        name: 'from',
+        type: 'varchar',
+        isNullable: false,
+      },
+      {
+        name: 'subject',
+        type: 'varchar',
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable(this.table);
+  }
+}
